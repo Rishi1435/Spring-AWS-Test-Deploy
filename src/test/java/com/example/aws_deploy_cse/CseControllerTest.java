c0719aeeb62fa9f28d04ed9638ea9ae73107b4a3
Rishi1435 <pediredlarishi2005@gmail.com>
2026-02-28 15:59:24 +0530

    added packages

--- src/test/java/com/example/aws_deploy_cse/CseControllerTest.java
+++ src/test/java/com/example/aws_deploy_cse/CseControllerTest.java
@@ -1,5 +1,8 @@
 package com.example.aws_deploy_cse;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
